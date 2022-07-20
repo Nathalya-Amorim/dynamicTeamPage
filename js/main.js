d1991c453e044a87031aacf3785bff5de8fdf397
Tallyta Triolo <tally.soares@gmail.com>
2022-07-20 17:14:06 -0400

    fixing bugs

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 (() => {
 
+    console.log('JS is working!');
+
     let nathButton = document.querySelector('#nathButton');
     let tallyButton = document.querySelector('#tallyButton');
     let closeButton = document.querySelector('#closeBtn');
 
-    function openOverlayPopUp(event) {
-        document.getElementById("overlayPopup").style.display = "block";
-        document.getElementById("memberProfile").src = "/img/" + event.target.dataset.member + "-profile.jpg";
-    }
-
-    function closeOverlayPopUp(event) {
-        document.getElementById("overlayPopup").style.display = "none";
-    }
-
-
     nathButton.addEventListener('click', openOverlayPopUp);
     tallyButton.addEventListener('click', openOverlayPopUp);
     closeButton.addEventListener('click', closeOverlayPopUp);
-  
+
+    function openOverlayPopUp(event) {
+        document.getElementById("overlayPopup").style.display = "block";
+        document.getElementById("memberProfile").src = "./img/" + event.target.dataset.member + "-profile.jpg";
+    }
+
+    function closeOverlayPopUp() {
+        document.getElementById("overlayPopup").style.display = "none";
+    }
 
 })();
